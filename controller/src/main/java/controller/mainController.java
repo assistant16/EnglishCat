2bc67eef5467eac2f8ca9fd9fde5fff56a55ca9b
assisntant16 <blek.bogdanovich@mail.ru>
2019-08-01 00:30:11 +0300

    'add_cfg'

--- controller/src/main/java/controller/mainController.java
+++ controller/src/main/java/controller/mainController.java
@@ -1,8 +1,20 @@
 package controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
 
-@Controller
-public class mainController {
+
+@RestController()
+@RequestMapping(value = "/app")
+public class mainController    {
+
+    @GetMapping("/h")
+    public String printHello(){
+        System.out.println("hello w");
+        return "hello";
+    }
+
+
 }
---
+++ controller/src/main/java/init/WebConfiguration.java
@@ -0,0 +1,11 @@
+package init;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan()
+public class WebConfiguration implements WebMvcConfigurer {}
